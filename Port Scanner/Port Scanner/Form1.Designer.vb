7eb42a29afbcf522a16a144a0f6f03752241b3ea
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2020-08-06 16:52:57 +0200

    Add files via upload

---
+++ Port Scanner/Port Scanner/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.clock_hosts = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckedListBox2 = New System.Windows.Forms.CheckedListBox()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'clock_hosts
+        '
+        Me.clock_hosts.Enabled = True
+        Me.clock_hosts.Interval = 5000
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(432, 31)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(356, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(11, 60)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(224, 304)
+        Me.CheckedListBox1.TabIndex = 1
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(432, 60)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(356, 335)
+        Me.TreeView1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(432, 401)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(356, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start scan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(14, 401)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(223, 37)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Hosts discovery"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(11, 29)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(224, 25)
+        Me.ProgressBar2.TabIndex = 6
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(241, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Bases IP to use :"
+        '
+        'CheckedListBox2
+        '
+        Me.CheckedListBox2.FormattingEnabled = True
+        Me.CheckedListBox2.Location = New System.Drawing.Point(243, 60)
+        Me.CheckedListBox2.Name = "CheckedListBox2"
+        Me.CheckedListBox2.Size = New System.Drawing.Size(183, 334)
+        Me.CheckedListBox2.TabIndex = 8
+        '
+        'BackgroundWorker2
+        '
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(688, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 9
+        Me.TextBox1.Text = "8888"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(620, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Port range :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckedListBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Form1"
+        Me.Text = "Network Scanner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents clock_hosts As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckedListBox2 As CheckedListBox
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Port Scanner/Port Scanner/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+    Dim host As String
+    Dim port As Integer
+    Dim counter As Integer
+    Private Sub scan_hosts(ByVal ip As String)
+
+        Dim strIPAddress As String
+
+        strIPAddress = ip
+
+        Dim split As String() = strIPAddress.Split(".")
+        Dim base As String = strIPAddress
+        base = base.Replace(split(split.Length - 1), "")
+        Me.Invoke(Sub()
+                      'safe to access the form or controls in here
+                      Me.ProgressBar2.Maximum = 254
+                  End Sub)
+
+        For i = 1 To 254
+            Me.Invoke(Sub()
+                          'safe to access the form or controls in here
+                          ProgressBar2.Value = ProgressBar2.Value + 1
+                      End Sub)
+
+            If My.Computer.Network.Ping(base & i.ToString, 1) Then
+                Try
+                    Me.Invoke(Sub()
+                                  'safe to access the form or controls in here
+                                  Me.CheckedListBox1.Items.Add(System.Net.Dns.GetHostEntry(base & i.ToString).HostName.ToString)
+                              End Sub)
+                Catch ex As Exception
+                End Try
+
+            End If
+
+        Next
+
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim root = New TreeNode("Hosts")
+        TreeView1.Nodes.Add(root)
+        For Each host As String In Me.CheckedListBox1.CheckedItems
+            TreeView1.Nodes(0).Nodes.Add(New TreeNode(host))
+        Next
+        BackgroundWorker1.RunWorkerAsync()
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BackgroundWorker2.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim node As Integer = 0
+        Dim max As Integer = TextBox1.Text
+
+        If Me.CheckedListBox1.CheckedItems.Count() <> 1 Then
+
+            For Each host As String In Me.CheckedListBox1.CheckedItems
+
+                Me.Invoke(Sub()
+                              'safe to access the form or controls in here
+                              Me.ProgressBar1.Maximum = max
+                          End Sub)
+
+                For i = 0 To max
+                    Me.Invoke(Sub()
+                                  'safe to access the form or controls in here
+                                  ProgressBar1.Value = i
+                              End Sub)
+
+                    port = i
+                    Dim hostadd As System.Net.IPAddress =
+                System.Net.Dns.GetHostEntry(host).AddressList(0)
+                    Dim EPhost As New System.Net.IPEndPoint(hostadd, port)
+                    Dim s As New System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork,
+                                                       System.Net.Sockets.SocketType.Stream,
+                                                       System.Net.Sockets.ProtocolType.Tcp)
+                    Try
+                        s.Connect(EPhost)
+                    Catch
+                    End Try
+                    If s.Connected Then
+                        Me.Invoke(Sub()
+                                      'safe to access the form or controls in here
+                                      Me.TreeView1.Nodes(0).Nodes(node).Nodes.Add(i)
+                                  End Sub)
+
+                    End If
+
+                Next
+                node = node + 1
+            Next
+        Else
+            host = Me.CheckedListBox1.CheckedItems(0)
+            Me.Invoke(Sub()
+                          'safe to access the form or controls in here
+                          Me.ProgressBar1.Maximum = max
+                      End Sub)
+
+            For i = 0 To max
+                Me.Invoke(Sub()
+                              'safe to access the form or controls in here
+                              ProgressBar1.Value = i
+                          End Sub)
+
+                port = i
+                Dim hostadd As System.Net.IPAddress =
+            System.Net.Dns.GetHostEntry(host).AddressList(0)
+                Dim EPhost As New System.Net.IPEndPoint(hostadd, port)
+                Dim s As New System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork,
+                                                   System.Net.Sockets.SocketType.Stream,
+                                                   System.Net.Sockets.ProtocolType.Tcp)
+                Try
+                    s.Connect(EPhost)
+                Catch
+                End Try
+                If s.Connected Then
+                    Me.Invoke(Sub()
+                                  'safe to access the form or controls in here
+                                  Me.TreeView1.Nodes(0).Nodes(node).Nodes.Add(i)
+                              End Sub)
+
+                End If
+
+            Next
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim addrlist = System.Net.Dns.GetHostByName(System.Net.Dns.GetHostName()).AddressList().ToList
+        For Each ips In addrlist
+            CheckedListBox2.Items.Add(ips.ToString)
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker2_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker2.DoWork
+        For Each ips As String In Me.CheckedListBox2.CheckedItems
+            scan_hosts(ips)
+        Next
+    End Sub
+
+End Class
